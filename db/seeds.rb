0247a7ab64f4ea99f2c561676ed6957e23759fdc
Lina NEGOUAI <lina.negouai@wanadoo.fr>
2022-02-08 11:45:42 -0400

    generating seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require "open-uri"
+require "json"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# https://image.tmdb.org/t/p/w200 + poster_path
+
+response = URI.open("http://tmdb.lewagon.com/movie/top_rated").read
+movies = JSON.parse(response)["results"]
+movies.each do |movie|
+  Movie.create(title: movie["title"],
+               overview: movie["overview"],
+               poster_url: "https://image.tmdb.org/t/p/w200#{movie['poster_path']}",
+               rating: movie["vote_average"].round)
+end
