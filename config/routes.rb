4ab4e4cd2c4f36c54dfae011dd0c3e61538d0dc7
Lina NEGOUAI <lina.negouai@wanadoo.fr>
2022-02-09 12:15:04 -0400

    add list#delete and style

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :lists, only: %i[index create show] do
+  resources :lists, only: %i[index create show destroy] do
     resources :bookmarks, only: %i[new create]
   end
+
+  resources :bookmarks, only: %i[destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
